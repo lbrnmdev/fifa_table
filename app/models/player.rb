679d4989c6635fe7ac86f544e7d5da8c378aa6ae
lbrnm dev macbook-pro <lbrnm.dev@gmail.com>
2016-10-09 23:43:26 +0200

    calculate player winning percentage before saving player

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,2 +1,12 @@
 class Player < ApplicationRecord
+  before_save :calculate_winning_percentage
+
+  private
+
+    # winning percentage calculated as ratio of wins to games played
+    # drawn games are considered half a win
+    def calculate_winning_percentage
+      # won, lost, drawn, winning_percentage
+      self.winning_percentage = (self.won + (0.5*self.drawn))/(self.won + self.lost + self.drawn)
+    end
 end
