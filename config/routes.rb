9563b911a919b07af86948672f0012062cde0f55
lbrnm dev macbook-pro <lbrnm.dev@gmail.com>
2016-10-16 01:54:54 +0200

    gen players controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  get 'players/index'
+
+  get 'players/new'
+
+  get 'players/show'
+
+  get 'players/edit'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
